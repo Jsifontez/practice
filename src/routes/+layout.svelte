674ade8f748a0304995735a50e944c8bb8ef208d
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-09-10 20:50:05 -0400

    Add font family

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <slot />
+
+<style>
+	* {
+		font-size: border-box;
+	}
+</style>
